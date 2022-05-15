0399596d21c0d4e0c0cf1d56d7e4b58ba94e4500
gbabeux-dev <gabriel.babeux.1@ens.etsmtl.ca>
2022-05-15 11:55:55 -0400

    Transformed the tabs component into a more reusable component

--- src/components/Experience/WorkTabs.tsx
+++ src/components/Experience/WorkTabs.tsx
@@ -1,204 +1,71 @@
-import { Box, Tab, Tabs, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import * as React from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
-import styled from 'styled-components';
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
-
-const Container = styled.div`
-  backdrop-filter: blur(6px);
-`
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: number) {
-    return {
-        id: `vertical-tab-${index}`,
-        'aria-controls': `vertical-tabpanel-${index}`,
-    };
-}
+import CustomTabs from '../shared/CustomTabs';
 
 const useStyles = makeStyles({
-    tabs: {
-        minWidth: 150,
-    },
-    tab: {
-        fontSize: '1.8rem',
-        textTransform: "none",
-        "& .Mui-selected": {
-            color: 'var(--green) !important',
-        }
-    },
-    customTabIndicator: {
-        background: 'var(--green) !important'
-    },
-    tabPanel: {
+    tabPanelContent: {
         fontSize: '1.8rem',
     },
-    box: {
-        justifyContent: 'center'
+    techStack: {
+        fontSize: '1.4rem',
+        marginTop: '2rem',
+        color: 'var(--green) !important'
+    },
+    customLi: {
+        marginBottom: '1rem'
     }
 })
 
 export default function WorkTabs() {
     const classes = useStyles()
-    const [value, setValue] = React.useState(0);
-    const [width, setWidth] = React.useState<number>(window.innerWidth);
+    const tabsPanelsContent = [
+        <div className={classes.tabPanelContent} >
+            <ol>
+                <li className={classes.customLi}>1. Developed a product recommendation micro-service with Flask (using an IBCF algorithm and containerized with Docker).</li>
+                <li className={classes.customLi}>2. Developed multiple new features such as: an order history module, an imperial to metric unit converter, a retailer dashboard, a PDF micro-service, retailers maps and much more.</li>
+                <li className={classes.customLi}>3. Refactored multiple React class components to functional components in order to improve performance and maintainability.</li>
+                <li className={classes.customLi}>4. Wrote migration scripts in order to transit the database collections to a new model.</li>
+                <li className={classes.customLi}>5. Participated and contributed to the Agile Scrum environment.</li>
+            </ol>
+            <div className={classes.techStack}>
+                React, Typescript, Node.js, Redux, MongoDB, Python, Flask, Git, Docker, AWS
+            </div>
+        </div>
+        ,
+        <div className={classes.tabPanelContent}>
+            <ol>
+                <li className={classes.customLi}>1. Developed a magazine viewer in Javascript in order to be able to view magazines on the webapp.</li>
+                <li className={classes.customLi}>2. Developed a new feature to maintain logs of certain user actions and feedback (using Winston.js).</li>
+                <li className={classes.customLi}>3. Refactored a Wordpress plugin to be able to clean our media libraries on a regular basis to save disk space.</li>
+                <li className={classes.customLi}>4. Refactored a Trivia game in JS in order to add new features and allow multiple players to play simultaneously.</li>
+                <li className={classes.customLi}>5. Fixed multiple frontend and backend bugs on the platform</li>
+            </ol>
+            <div className={classes.techStack}>
+                Wordpress, Javascript, Node.js, PHP, HTML, CSS, Nginx
+            </div>
+        </div>
+        ,
+        <div className={classes.tabPanelContent} >
+            <ol>
+                <li className={classes.customLi}>1. Developed a reservation module for a spa company using Javascript, HTML, CSS for the frontend and SQL for the backend queries.</li>
+                <li className={classes.customLi}>2. Created multiple complex SQL queries, functions and stored procedures required for certain modules.</li>
+                <li className={classes.customLi}>3. Developed API endpoints for sensors that are using the MQTT protocol in order to feed information to a dashboard.</li>
+            </ol>
+            <div className={classes.techStack}>
+                C#, SQL, ASP.NET, Javascript, HTML, CSS
+            </div>
+        </div>
+    ]
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-        setValue(newValue);
-    };
-
-    const handleWindowSizeChange = () => {
-        setWidth(window.innerWidth);
-    }
-
-    React.useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange);
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange);
-        }
-    }, []);
-
-    const tabPanels = (
-        <>
-            <TabPanel value={value} index={0}>
-                <div className={classes.tabPanel} >
-                    1. Developed a product recommendation micro-service with Flask (using an IBCF algorithm and
-                    containerized with Docker).
-                    <br />
-                    2. Developed multiple new features such as: an order history module, an imperial to metric unit converter,
-                    a retailer dashboard, a PDF micro-service, retailers maps and much more.
-                    <br />
-                    3. Refactored multiple React class components to functional components in order to improve
-                    performance and maintainability.
-                    <br />
-                    4. Wrote migration scripts in order to transit the database collections to a new model.
-                    <br />
-                    5. Participated and contributed to the Agile Scrum environment.
-                    Tech stack: (React, Typescript, Node.js, Redux, MongoDB, Python, Flask, Git, Docker, AWS)
-                </div>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                <div className={classes.tabPanel} >
-                    1. Developed a product recommendation micro-service with Flask (using an IBCF algorithm and
-                    containerized with Docker).
-                    <br />
-                    2. Developed multiple new features such as: an order history module, an imperial to metric unit converter,
-                    a retailer dashboard, a PDF micro-service, retailers maps and much more.
-                    <br />
-                    3. Refactored multiple React class components to functional components in order to improve
-                    performance and maintainability.
-                    <br />
-                    4. Wrote migration scripts in order to transit the database collections to a new model.
-                    <br />
-                    5. Participated and contributed to the Agile Scrum environment.
-                    Tech stack: (React, Typescript, Node.js, Redux, MongoDB, Python, Flask, Git, Docker, AWS)
-                </div>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                <div className={classes.tabPanel} >
-                    1. Developed a product recommendation micro-service with Flask (using an IBCF algorithm and
-                    containerized with Docker).
-                    <br />
-                    2. Developed multiple new features such as: an order history module, an imperial to metric unit converter,
-                    a retailer dashboard, a PDF micro-service, retailers maps and much more.
-                    <br />
-                    3. Refactored multiple React class components to functional components in order to improve
-                    performance and maintainability.
-                    <br />
-                    4. Wrote migration scripts in order to transit the database collections to a new model.
-                    <br />
-                    5. Participated and contributed to the Agile Scrum environment.
-                    Tech stack: (React, Typescript, Node.js, Redux, MongoDB, Python, Flask, Git, Docker, AWS)
-                </div>
-            </TabPanel>
-        </>
-    )
-
-    const tabs = (
+    const tabsNames = (
         [
-            <Tab label={<span className={classes.tab}>Pivohub</span>} {...a11yProps(0)} />,
-            <Tab label={<span className={classes.tab}>Ongoing</span>} {...a11yProps(1)} />,
-            <Tab label={<span className={classes.tab}>Pheidi</span>} {...a11yProps(2)} />
+            "Pivohub",
+            "Ongoing",
+            "Pheidi"
         ]
     )
 
-    const isMobile = width <= 820;
-
     return (
-        <Container>
-            <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-                {!isMobile && (
-                    <Box
-                        sx={{ flexGrow: 1, bgcolor: '#ffffff05', display: 'flex' }}
-                    >
-                        <Tabs
-                            className={classes.tabs}
-                            orientation="vertical"
-                            variant="fullWidth"
-                            value={value}
-                            onChange={handleChange}
-                            aria-label="Vertical tabs"
-                            sx={{ borderRight: 1, borderColor: 'divider' }}
-                            classes={{
-                                root: classes.tab,
-                                indicator: classes.customTabIndicator
-                            }}
-                        >
-                            {tabs}
-                        </Tabs>
-                        {tabPanels}
-                    </Box>
-                )}
-
-                {isMobile && (
-                    <>
-                        <Box sx={{ flexGrow: 1, bgcolor: '#ffffff05', display: 'flex', justifyContent: 'center' }}>
-                            <Tabs
-                                className={classes.tabs}
-                                orientation="horizontal"
-                                value={value}
-                                onChange={handleChange}
-                                aria-label="Vertical tabs"
-                                classes={{
-                                    root: classes.tab,
-                                    indicator: classes.customTabIndicator
-                                }}
-                                sx={{ borderBottom: 1, borderColor: 'divider' }}
-                            >
-                                {tabs}
-                            </Tabs>
-                        </Box>
-                        <Box className={classes.box} sx={{ flexGrow: 1, bgcolor: '#ffffff05', display: 'flex' }}>
-                            {tabPanels}
-                        </Box>
-                    </>
-                )}
-            </ScrollAnimation>
-        </Container>
+        <CustomTabs tabsNames={tabsNames} tabsPanelsContent={tabsPanelsContent} />
     );
 }
---
+++ src/components/shared/CustomTabs.tsx
@@ -0,0 +1,120 @@
+import { Box, Tab, Tabs } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import * as React from 'react';
+import ScrollAnimation from 'react-animate-on-scroll';
+import TabPanel from './TabPanel';
+
+function tabProps(index: number) {
+    return {
+        id: `tab-${index}`,
+        'aria-controls': `tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles({
+    tabs: {
+        minWidth: 150,
+    },
+    tab: {
+        fontSize: '1.8rem',
+        textTransform: "none",
+        "& .Mui-selected": {
+            color: 'var(--green) !important',
+        }
+    },
+    customTabIndicator: {
+        background: 'var(--green) !important'
+    },
+    container: {
+        backdropFilter: 'blur(6px)'
+    }
+})
+
+interface Props {
+    tabsPanelsContent: JSX.Element[]
+    tabsNames: String[]
+}
+
+export default function CustomTabs(props: Props) {
+    const classes = useStyles()
+    const [value, setValue] = React.useState(0);
+    const [width, setWidth] = React.useState<number>(window.innerWidth);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const handleWindowSizeChange = () => {
+        setWidth(window.innerWidth);
+    }
+
+    React.useEffect(() => {
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
+    }, []);
+
+    const isMobile = width <= 820;
+
+    const tabsPanels = props.tabsPanelsContent.map((tabPanelContent, index) => (
+        <TabPanel value={value} index={index}>{tabPanelContent}</TabPanel>
+    ))
+
+    const tabs = props.tabsNames.map((tabName, index) => (
+        <Tab label={<span className={classes.tab}>{tabName}</span>} {...tabProps(index)} />
+    ))
+
+    return (
+        <div className={classes.container}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
+                {!isMobile && (
+                    <Box
+                        sx={{ flexGrow: 1, bgcolor: '#ffffff05', display: 'flex' }}
+                    >
+                        <Tabs
+                            className={classes.tabs}
+                            orientation="vertical"
+                            variant="fullWidth"
+                            value={value}
+                            onChange={handleChange}
+                            aria-label="Vertical tabs"
+                            sx={{ borderRight: 1, borderColor: 'divider' }}
+                            classes={{
+                                root: classes.tab,
+                                indicator: classes.customTabIndicator
+                            }}
+                        >
+                            {tabs}
+                        </Tabs>
+                        {tabsPanels}
+                    </Box>
+                )}
+
+                {isMobile && (
+                    <>
+                        <Box sx={{ flexGrow: 1, bgcolor: '#ffffff05', display: 'flex', justifyContent: 'center' }}>
+                            <Tabs
+                                className={classes.tabs}
+                                orientation="horizontal"
+                                value={value}
+                                onChange={handleChange}
+                                aria-label="Horizontal tabs"
+                                classes={{
+                                    root: classes.tab,
+                                    indicator: classes.customTabIndicator
+                                }}
+                                sx={{ borderBottom: 1, borderColor: 'divider' }}
+                            >
+                                {tabs}
+                            </Tabs>
+                        </Box>
+                        <Box sx={{ flexGrow: 1, bgcolor: '#ffffff05', display: 'flex' }}>
+                            {tabsPanels}
+                        </Box>
+                    </>
+                )}
+            </ScrollAnimation>
+        </div>
+    );
+}
